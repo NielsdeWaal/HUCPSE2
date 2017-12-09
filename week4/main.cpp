fcc48e9308ffc58e91c1ad7eb3fd5bc1fa1fda70
Niels <nielsdwaal@gmail.com>
2017-12-09 21:42:22 +0100

    More progress on week 4

--- week4/main.cpp
+++ week4/main.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <iterator>
 #include <map>
+#include <ctype.h>
 
 auto countCharacters(const std::vector<char>& countVector) {
     std::map<char, int> resultMap;
@@ -23,19 +24,35 @@ auto fillVector(const auto& filename, std::vector<char>& destVector) {
               std::back_inserter(destVector));
 }
 
+auto fillWordVector(const auto& filename, std::vector<std::string>& destVector) {
+    std::ifstream file("bible.txt");
+
+    std::copy(std::istream_iterator<std::string>(file),
+              std::istream_iterator<std::string>(),
+              std::back_inserter(destVector));
+}
+
 int main() {
     constexpr const auto filename = "bible.txt";
     std::vector<char> characterVector;
+    std::vector<std::string> wordVector;
 
     fillVector(filename, characterVector);
+    fillWordVector(filename, wordVector);
+
     auto lines = std::count(characterVector.begin(), characterVector.end(), '\n');
     auto alphabeticCharacters = std::count_if(characterVector.begin(),
                                               characterVector.end(),
-                                              [](char c){return ('a' <= c <= 'z')||('A' <= c <= 'Z');});
+                                              [](char c){return /*('a' <= c <= 'z')||('A' <= c <= 'Z');*/ isalpha(c);});
     std::transform(characterVector.begin(), characterVector.end(), characterVector.begin(),
                     [](char c) -> char {return std::tolower(c);});
 
     auto characterCount = countCharacters(characterVector);
+    std::map<int, char> countOrderMap;
+
+    for (auto&& [key, value] : characterCount) {
+        countOrderMap[value] = key;
+    }
 
     try {
         std::cout << "Vector size: " << characterVector.size() << "\n";
@@ -47,9 +64,8 @@ int main() {
             std::cout << "Char: " << key << ", value: " << value << "\n";
         }
 
-        
 
-        for (auto&& [key,value] : characterCount) {
+        for (auto&& [key,value] : countOrderMap) {
             std::cout << "Char: " << key << ", value: " << value << "\n";
         }
 
